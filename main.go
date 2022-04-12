70a721533896fabd8cbe3d24fd86a0a32a83b509
daniel.moraes <daniel.moraes@tradetechnology.com.br>
2022-04-12 16:20:37 -0300

    Creating menu

--- main.go
+++ main.go
@@ -6,12 +6,22 @@ func main() {
 	version := 1.0
 
 	fmt.Println("Welcome to the WebSites Monitor version", version)
-	fmt.Println("1 - Initialize monitoring")
+	fmt.Println("1 - Start monitoring")
 	fmt.Println("2 - Show logs")
-	fmt.Println("3 - Exit")
+	fmt.Println("0 - Exit")
 
 	var menuOption int
 
 	fmt.Scan(&menuOption) //The simbol "&" means that menuOption is a pointer of memory
 	fmt.Println("The menu you chose is", menuOption)
+
+	if menuOption == 1 {
+		fmt.Println("Starting monitoring...")
+	} else if menuOption == 2 {
+		fmt.Println("Showing logs...")
+	} else if menuOption == 0 {
+		fmt.Println("Exiting...")
+	} else {
+		fmt.Println("Invalid option!")
+	}
 }
